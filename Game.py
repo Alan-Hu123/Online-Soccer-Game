26a7b1c6991f93730a69a1719a1739d0d9c232d5
Alan Hu <64699846+Ailun-Hu@users.noreply.github.com>
2024-02-18 15:38:01 -0500

    added Chat Box

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from collections import deque
 import pygame
 pygame.font.init()
 
@@ -9,15 +10,22 @@ HEIGHT = 960
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Online Soccer Game")
 
-BG = pygame.transform.scale(pygame.image.load("CjJCu.png"), (WIDTH, HEIGHT))
+BG = pygame.transform.scale(pygame.image.load("Background.png"), (WIDTH, HEIGHT))
 
-PLAYER_RADIUS = 35
-PLAYER_VEL = 5
+PLAYER_RADIUS = 30
+PLAYER_VEL = 4
 BALL_RADIUS = 25
 
 FONT = pygame.font.SysFont("comicsans", 30)
+CHAT_FONT = pygame.font.SysFont("comicsans", 15)
+CHAT_HEIGHT = 200
+CHAT_WIDTH = 300
+CHAT_POSX = WIDTH - CHAT_WIDTH + 40
+CHAT_POSY = HEIGHT - CHAT_HEIGHT + 20
+chat_box = pygame.Rect(WIDTH-CHAT_WIDTH, HEIGHT-CHAT_HEIGHT, CHAT_WIDTH, CHAT_HEIGHT)
 
-ball = [0, 0]
+queue = deque()
+
 class Player:
     def __init__(self, x, y):
         self.x = x
@@ -28,8 +36,13 @@ class Ball:
         self.y = y
         self.velx = 0
         self.vely = 0
+    def reset(self):
+        self.x = WIDTH/2
+        self.y = HEIGHT/2
+        self.velx = 0
+        self.vely = 0
 
-        
+   
 def collide(player, ball):
     if (sqrt(((player.x - ball.x)**2) + ((player.y - ball.y) ** 2)) <= (PLAYER_RADIUS + BALL_RADIUS)):
         ball.velx = (ball.x - player.x)
@@ -38,16 +51,22 @@ def collide(player, ball):
 def wallCollision(ball):
     if(ball.x + ball.velx > 1500 - BALL_RADIUS or ball.x + ball.velx < 78 + BALL_RADIUS ):
         ball.velx *= -1
+        if(ball.y > 430 and ball.y < 530):
+            ball.reset()
     if(ball.y + ball.vely > 935 - BALL_RADIUS or ball.x + ball.velx < 17 + BALL_RADIUS):
         ball.vely *= -1
    
 
-def draw(player, ball):
+def draw(player, ball, chat):
     WIN.blit(BG, (0, 0))
 
-
+    pygame.draw.rect(WIN, "red", chat_box)
+    for i in range(0, 3):
+        text = CHAT_FONT.render(chat[i], 1, "white")
+        WIN.blit(text, (CHAT_POSX, CHAT_POSY + 40 * i))
     pygame.draw.circle(WIN, "black" , (player.x, player.y), PLAYER_RADIUS)
     pygame.draw.circle(WIN, "gray" , (ball.x, ball.y), BALL_RADIUS)
+
     pygame.display.update()
 
 
@@ -58,6 +77,10 @@ def main():
     ball = Ball(WIDTH/2, HEIGHT/2)
     clock = pygame.time.Clock()
 
+    
+    queue.append("TEST 1")
+    queue.append("TEST 2")
+    queue.append("TEST 3")
     while run:
         clock.tick(60)
         ball.velx *= 0.85
@@ -82,7 +105,10 @@ def main():
         ball.x += ball.velx
         ball.y += ball.vely
 
-        draw(player, ball)
+
+        # l = [str(player.x), str(player.y)]
+        # print(l[0] + " " + l[1])
+        draw(player, ball, queue)
     
 
     
